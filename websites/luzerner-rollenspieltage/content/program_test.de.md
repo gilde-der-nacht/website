16bbf7fa1e2f335c0861c183e5590e514ea37319
Thomas Stauffer <thomasstauffer@gmx.ch>
2019-07-21 21:20:09 +0200

    many Apollon improvements
    - submits all rounds
    - filters multiple registrations
    - overview, ordered by day, time, name
    - hint overlapping rounds - inside round
    - hint overlapping rounds - close to summary
    - apollon ids have a "apollon-" prefix
    - split rounds into friday/saturday (at the moment does not detect days itself)

--- websites/luzerner-rollenspieltage/content/program_test.de.md
+++ websites/luzerner-rollenspieltage/content/program_test.de.md
@@ -6,15 +6,16 @@ toc: true
 
 Suche dir die Spielrunden aus, bei denen du gerne teilnehmen möchtest und schicke ganz unten das Formular ab:
 
-<template id="i18n">
+<template id="apollon-i18n">
     <p data-id="friday" data-text="Freitag">-</p>
     <p data-id="saturday" data-text="Samstag">-</p>
     <!-- e.g. countryflags.com -->
     <p data-id="flag-url-DE" data-text="/graphics/germany-flag-small.png">-</p>
     <p data-id="flag-url-EN" data-text="/graphics/united-kingdom-flag-small.png">-</p>
+    <p data-id="overlapping" data-text="Achtung: Mindestens zwei Spielrunden überlappen zeitlich!">-</p>
 </template>
 
-<template id="round-template">
+<template id="apollon-round-template">
     <label data-id="container">
         <div class="round">
             <h1><span data-id="name"></span></h1>
@@ -26,27 +27,25 @@ Suche dir die Spielrunden aus, bei denen du gerne teilnehmen möchtest und schic
             <p>Spieler Aktuell: <strong><span data-id="players-current"></span></strong> / Max: <span data-id="players-max"></span></p>
             <input data-id="checkbox" type="checkbox">
             <p><input data-id="btn-choose" class="c-btn" type="button"></p>
-            <p class="hint"><span data-id="hint"></span></p>
+            <p class="hint" style="background-color: #f9f"><span data-id="hint"></span></p>
         </div>
     </label>
 </template>
 
-<template id="summary-template">
+<template id="apollon-summary-template">
     <p data-id="container">Name: <span data-id="name"></span> / Tag <span data-id="day"></span> / Von <span data-id="from"></span> / Bis <span data-id="to"></span></p>
 </template>
 
-# Freitag, 30. August 2019
-
-<div id="c-rounds" class="u-bleed-out"></div>
-
-# Samstag, 31. August 2019
-
-<pre>TODO: Tage aufteilen</pre>
+<div id="apollon-rounds">
+    <h1>Freitag, 30. August 2019</h1>
+    <div id="apollon-rounds-friday" class="u-bleed-out c-rounds"></div>
+    <h1>Samstag, 31. August 2019</h1>
+    <div id="apollon-rounds-saturday" class="u-bleed-out c-rounds"></div>
+</div>
 
 # Anmeldung abschliessen
 
 <div class="c-form">
-<form action="https://formsubmit.co/mail@rollenspieltag.ch" method="POST">
     <div>
         <p class="c-form--item c-form-field--text">
             <label for="name">Name *</label>
@@ -63,14 +62,15 @@ Suche dir die Spielrunden aus, bei denen du gerne teilnehmen möchtest und schic
 
 <h2>Zusammenfassung</h2>
 
-<div id="summaries"></div>
+
+<p id="apollon-summary-hint" style="background-color: #f9f"></p>
+<div id="apollon-summary"></div>
         <input class="c-btn" type="submit" id="submit" value="Absenden">
         <input type="hidden" name="_next" value="http://localhost:1313/program_test/">
         <input type="hidden" name="_captcha" value="false">
     </div>
-</form>
-</div>
 
+</div>
 
 
 <script src="/scripts/apollon-model.js"></script>
--- websites/luzerner-rollenspieltage/static/scripts/apollon-view.js
+++ websites/luzerner-rollenspieltage/static/scripts/apollon-view.js
@@ -1,15 +1,33 @@
 'use strict';
 
+function replaceWithFragment(node, fragment) {
+    node.innerHTML = ''; // ugly?
+    node.appendChild(fragment);
+}
+
+// TODO prefix important ids with apollon-
 async function main() {
     const registrations = await getRegistrations();
 
     const i18nMap = {};
-    document.getElementById('i18n').content.querySelectorAll('*').forEach(child => {
+    document.getElementById('apollon-i18n').content.querySelectorAll('*').forEach(child => {
         i18nMap[child.dataset.id] = child.dataset.text;
     });
 
     function translate(what) {
         return i18nMap[what];
+    }
+    
+    function getSelectedRounds() {
+        const roundsNode = document.getElementById('apollon-rounds');
+        const containers = [...roundsNode.querySelectorAll('[data-id=container]')];
+        return containers.reduce((accumulator, roundNode) => {
+            const checkboxNode = roundNode.querySelector('[data-id=checkbox]');
+            if(checkboxNode.checked === true) {
+                accumulator.push(roundNode.dataset.roundId);
+            }
+            return accumulator;
+        }, []);
     }
 
     async function submit() {
@@ -19,7 +37,8 @@ async function main() {
         const name = nameNode.value;
         const email = emailNode.value;
         const comment = commentNode.value;
-        await submitRegistration(name, email, comment);
+        const rounds = selectedRounds();
+        await registrationAdd(name, email, comment, rounds);
     }
 
     function updateRoundState(roundNode, playersCurrent, playersMax, checkboxNode) {
@@ -36,55 +55,61 @@ async function main() {
     }
 
     function showSummaries(event) {
-        const summaryTemplateNode = document.getElementById('summary-template');
-        const summariesNode = document.getElementById('summaries');
-        const roundsNode = document.getElementById('c-rounds');
+        const summaryTemplateNode = document.getElementById('apollon-summary-template');
+        const roundsNode = document.getElementById('apollon-rounds');
         const fragment = document.createDocumentFragment();
-        forEachRound(registrations, (roundId, round, game, playersMax, playersCurrent) => {
-            const roundNode = roundsNode.querySelector('[data-round-id="' + roundId + '"]');
+        const selectedRoundIds = getSelectedRounds();
+        const overlappingRoundIds = roundsDayTimeOverlapping(selectedRoundIds);
+        let overlapping = overlappingRoundIds.length > 0;
+        roundsDetailed(registrations).forEach(entry => {
+            const roundNode = roundsNode.querySelector('[data-round-id="' + entry.roundId + '"]');
             const checkboxNode = roundNode.querySelector('[data-id=checkbox]');
             const hintNode = roundNode.querySelector('[data-id=hint]');
-            hintNode.innerText = checkboxNode.checked ? 'SELECTED' : 'NOT SELECTED?'; // TODO do something useful here or remove it
+            hintNode.innerText = overlappingRoundIds.includes(entry.roundId) ? translate('overlapping') : '';
             if(checkboxNode.checked === true) {
                 const templateNode = summaryTemplateNode.content.cloneNode(true);
-                templateNode.querySelector('[data-id=name]').innerText = game.name;
-                templateNode.querySelector('[data-id=day]').innerText = translate(round.day);
-                templateNode.querySelector('[data-id=from]').innerText = round.from;
-                templateNode.querySelector('[data-id=to]').innerText = round.to;
+                templateNode.querySelector('[data-id=name]').innerText = entry.game.name;
+                templateNode.querySelector('[data-id=day]').innerText = translate(entry.round.day);
+                templateNode.querySelector('[data-id=from]').innerText = entry.round.from;
+                templateNode.querySelector('[data-id=to]').innerText = entry.round.to;
                 fragment.appendChild(templateNode);
             }
-
-            updateRoundState(roundNode, playersCurrent, playersMax, checkboxNode)
+            updateRoundState(roundNode, entry.playersCurrent, entry.game.playersMax, checkboxNode)
         });
-
-        replaceWithFragment(summariesNode, fragment);
+        document.getElementById('apollon-summary-hint').innerText = overlapping ? translate('overlapping') : '';
+        replaceWithFragment(document.getElementById('apollon-summary'), fragment);
     }
 
+    /*
+    For every round, copy an "<template>", populate it with interesting information,
+    then put all elements into the browser.
+    */
     function showRounds() {
-        const roundTemplateNode = document.getElementById('round-template');
-        const roundsNode = document.getElementById('c-rounds');
-        const fragment = document.createDocumentFragment();
-
-        forEachRound(registrations, (roundId, round, game, playersMax, playersCurrent) => {
+        const roundTemplateNode = document.getElementById('apollon-round-template');
+        const fragment = {
+            'friday': document.createDocumentFragment(),
+            'saturday': document.createDocumentFragment(),
+        };
+        roundsDetailed(registrations).forEach(entry => {
             const templateNode = roundTemplateNode.content.cloneNode(true);
-            const checkboxNode = templateNode.querySelector('[data-id=checkbox]');
-            templateNode.querySelector('[data-id=container]').dataset.roundId = roundId;
-            templateNode.querySelector('[data-id=name]').innerText = game.name;
-            templateNode.querySelector('[data-id=gm]').innerText = game.gm;
-            templateNode.querySelector('[data-id=game-description]').innerText = game.gameDescription;
-            templateNode.querySelector('[data-id=campaign-description]').innerText = game.campaignDescription;
-            templateNode.querySelector('[data-id=lang]').innerText = game.lang;
-            templateNode.querySelector('[data-id=lang-img]').src = translate('flag-url-' + game.lang);
-            templateNode.querySelector('[data-id=day]').innerText = translate(round.day);
-            templateNode.querySelector('[data-id=from]').innerText = round.from;
-            templateNode.querySelector('[data-id=to]').innerText = round.to;
-            templateNode.querySelector('[data-id=players-current]').innerText = playersCurrent;
-            templateNode.querySelector('[data-id=players-max]').innerText = game.playersMax;
-            checkboxNode.addEventListener('change', showSummaries);
-            fragment.appendChild(templateNode);
+            templateNode.querySelector('[data-id=container]').dataset.roundId = entry.roundId;
+            templateNode.querySelector('[data-id=name]').innerText = entry.game.name;
+            templateNode.querySelector('[data-id=gm]').innerText = entry.game.gm;
+            templateNode.querySelector('[data-id=game-description]').innerText = entry.game.gameDescription;
+            templateNode.querySelector('[data-id=campaign-description]').innerText = entry.game.campaignDescription;
+            templateNode.querySelector('[data-id=lang]').innerText = entry.game.lang;
+            templateNode.querySelector('[data-id=lang-img]').src = translate('flag-url-' + entry.game.lang);
+            templateNode.querySelector('[data-id=day]').innerText = translate(entry.round.day);
+            templateNode.querySelector('[data-id=from]').innerText = entry.round.from;
+            templateNode.querySelector('[data-id=to]').innerText = entry.round.to;
+            templateNode.querySelector('[data-id=players-current]').innerText = entry.playersCurrent;
+            templateNode.querySelector('[data-id=players-max]').innerText = entry.game.playersMax;
+            templateNode.querySelector('[data-id=checkbox]').addEventListener('change', showSummaries);
+            fragment[entry.round.day].appendChild(templateNode);
         });
-
-        replaceWithFragment(roundsNode, fragment);
+        Object.keys(fragment).forEach(day => {
+            replaceWithFragment(document.getElementById('apollon-rounds-' + day), fragment[day]);
+        });
     }
 
     document.getElementById('submit').addEventListener('click', submit);
