149bb666f0e8fa541c10ecdab66d3bf97400649a
Oliver Bucher <level8broccoli@gmail.com>
2020-01-23 00:43:27 +0100

    no longer link to image on facebook

--- websites/gildedernacht.ch/static/scripts/cal-view.js
+++ websites/gildedernacht.ch/static/scripts/cal-view.js
@@ -45,7 +45,7 @@ const calendarMain = async () => {
                 const title = entry.summary;
                 let image = '';
                 if (title.includes('Spieltreffen')) {
-                    image = 'https://scontent.fzrh3-1.fna.fbcdn.net/v/t1.0-9/s960x960/69694518_1216457091891380_8506334758685376512_o.jpg?_nc_cat=105&_nc_ohc=pud3E5jHmvMAQkneL2We_HdGFVn1JMjtxHPuUmpgqJzocUuB_sqM-eTFA&_nc_ht=scontent.fzrh3-1.fna&oh=10828d4cb8514aeaf0acaef5e7205580&oe=5E4E6370';
+                    image = '/images/calendar-spieltreffen.jpg';
                 } else {
                     image = '/images/calendar-filler.jpg';
                 }
