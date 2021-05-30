20ba3ed4f7e9ff901ae5d788d63a25be79ff2be1
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-05-30 18:33:20 +0200

    update

--- websites/rollenspieltage.ch/static/scripts/apollon.js
+++ websites/rollenspieltage.ch/static/scripts/apollon.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const player = document.querySelector("#participant-role");
-const gameMaster = document.querySelector("#gamemaster-role");
+const player = document.querySelector('#participant-role');
+const gameMaster = document.querySelector('#gamemaster-role');
 
 const show = (element) => {
-    element.removeAttribute("hidden");
+    element.removeAttribute('hidden');
 };
 
 const hide = (element) => {
-    element.setAttribute("hidden", "true");
+    element.setAttribute('hidden', 'true');
 };
 
 const manageVisibility = () => {
-    const playerFieldset = document.querySelector("#player-fieldset");
-    const gameMasterFieldset = document.querySelector("#gamemaster-fieldset");
-    const noRoleSelectedFieldset = document.querySelector("#no-role-selected-fieldset");
+    const playerFieldset = document.querySelector('#player-fieldset');
+    const gameMasterFieldset = document.querySelector('#gamemaster-fieldset');
+    const noRoleSelectedFieldset = document.querySelector('#no-role-selected-fieldset');
 
     if (player.checked) {
         show(playerFieldset);
@@ -35,6 +35,14 @@ const manageVisibility = () => {
     }
 };
 
-player.addEventListener("change", manageVisibility);
-gameMaster.addEventListener("change", manageVisibility);
+player.addEventListener('change', manageVisibility);
+gameMaster.addEventListener('change', manageVisibility);
 manageVisibility();
+
+const sendRegistration = (e) => {
+    e.preventDefault();
+    console.log('submit');
+};
+
+const submitButton = document.querySelector('[type=submit]');
+submitButton.addEventListener('click', sendRegistration);
