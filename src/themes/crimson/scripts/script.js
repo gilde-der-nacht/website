bb54462ca9c0f8aeee1e5dcec44757b6395c2359
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-01-14 18:39:25 +0000

    theme switcher

--- src/themes/crimson/scripts/script.js
+++ src/themes/crimson/scripts/script.js
@@ -1,5 +1,7 @@
 import { toggleScrolling } from "./_mobile-menu.js";
+import { toggleTheme } from "./_theme-switcher.js";
 
 (function initialize() {
     toggleScrolling();
+    toggleTheme();
 }());
