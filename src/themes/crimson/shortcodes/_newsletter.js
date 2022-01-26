caeeb76f38aea1fd03996520994e6e52b8b95735
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-01-26 23:29:32 +0000

    add base markup for newsletter

--- src/themes/crimson/shortcodes/_newsletter.js
+++ src/themes/crimson/shortcodes/_newsletter.js
@@ -1,5 +1,59 @@
-function NewsletterForm() {
+const html = require("./helper/html");
 
+function NewsletterForm() {
+    return html`
+<section>
+    <form action="https://gildedernacht.us9.list-manage.com/subscribe/post?u=ac8c826d7db864c54a3c2f001&amp;id=c6bec31754" method="post">
+        <h1>Erinnerungs-Newsletter</h1>
+
+        <label>E-Mail-Adresse<input type="email" value="" name="EMAIL" placeholder="E-Mail-Adresse" required=""></label>
+        <label>Vorname<input type="text" value="" name="FNAME" placeholder="Vorname" required=""></label>
+        <label>Nachname<input type="text" value="" name="LNAME" placeholder="Nachname" required=""></label>
+
+        <h2>Thema</h2>
+        <p>Wähle mindestens ein Thema aus, das dich interessiert. Ansonsten wirst du keine E-Mails erhalten.</p>
+        <ul>
+            <li>
+                <label>
+                    <input type="checkbox" value="128" name="group[48105][128]">
+                    Brettspiele
+                </label>
+            </li>
+            <li>
+                <label>
+                    <input type="checkbox" value="256" name="group[48105][256]">
+                    Rollenspiele
+                </label>
+            </li>
+            <li>
+                <label>
+                    <input type="checkbox" value="512" name="group[48105][512]">
+                    Tabletop
+                </label>
+            </li>
+        </ul>
+
+        <h2>Häufigkeit</h2>
+        <p>Wenn du dich für weniger häufige E-Mails entscheidest, werden wir dich hauptsächlich über grössere Events informieren.</p>
+        <ul>
+            <li>
+                <label>
+                    <input type="radio" value="1024" name="group[48109]">
+                    max. 2/Monat
+                </label>
+            </li>
+            <li>
+                <label>
+                    <input type="radio" value="2048" name="group[48109]">
+                    max. 8/Jahr
+                </label>
+            </li>
+        </ul>
+
+        <button type="submit" class="button-accent">Abonnieren</button>
+    </form>
+</section>
+    `;
 }
 
 module.exports = { NewsletterForm };
---
+++ src/websites/gildedernacht.ch/content/newsletter.md
@@ -0,0 +1,10 @@
+---
+layout: default
+title: Newsletter
+eleventyNavigation:
+  key: newsletter
+  title: Newsletter
+  order: 5
+---
+
+{% NewsletterForm %}
