7dde1e00496b99b99310505eceacffeec8e799c5
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-02-01 20:19:33 +0000

    improve html helper function

--- src/themes/crimson/shortcodes/helper/html.js
+++ src/themes/crimson/shortcodes/helper/html.js
@@ -1,7 +1,7 @@
 function html(strings, ...expr) {
     return strings
         .reduce((acc, curr, i) => acc + expr[i - 1] + curr)
-        .replace(/\n\s*/g, "");
+        .replace(/\n\s*/g, " ");
 }
 
 module.exports = html;
