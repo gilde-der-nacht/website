c69c213735ac92df97916cd38fa297568bfab6bb
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-02-01 22:31:02 +0000

    generate images but no markup yet

--- src/themes/crimson/plugins/_markdown-image.js
+++ src/themes/crimson/plugins/_markdown-image.js
@@ -1,4 +1,37 @@
+const path = require('path');
+const Image = require("@11ty/eleventy-img");
+
+function generateImages({ src, outputDir, alt }) {
+    console.log({ src, outputDir });
+    const options = {
+        widths: [600],
+        formats: ["avif", "jpeg"],
+        outputDir: outputDir
+    };
+    Image(src, options);
+    const metadata = Image.statsSync(src, options);
+
+    // let imageAttributes = {
+    //   alt,
+    //   sizes: "100vw",
+    //   loading: "lazy",
+    //   decoding: "async",
+    // };
+    // return Image.generateHTML(metadata, imageAttributes);
+}
+
 function markdownImagePlugin(md, params) {
+    const before = md.renderer.rules.image;
+    md.renderer.rules.image = function (tokens, idx, options, env, self) {
+        var token = tokens[idx];
+        const srcAttr = token.attrs[token.attrIndex("src")][1];
+        const alt = token.content;
+        const { inputPath, outputPath } = env.page;
+        const src = path.join(path.dirname(inputPath), srcAttr);
+        const outputDir = path.join(path.dirname(outputPath), path.dirname(srcAttr));
+        generateImages({ src, outputDir, alt })
+        return before(tokens, idx, options, env, self);
+    }
 }
 
 module.exports = { markdownImagePlugin };
