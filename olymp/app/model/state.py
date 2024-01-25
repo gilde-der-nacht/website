bbaeb53c31129927168c05a22b13140bf4f422a4
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-01-25 21:23:21 +0000

    remove olymp/fastAPI project (never completed)

--- olymp/app/model/state.py
+++
@@ -1,9 +0,0 @@
-"""Imports"""
-from enum import Enum
-
-
-class State(str, Enum):
-    """Instead of deleting entries in the database, they get set to `inactive`."""
-
-    ACTIVE = "active"
-    INACTIVE = "inactive"
--- olymp/start.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-# Run migrations
-alembic upgrade head
-
-# Start server
-exec uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port 80
