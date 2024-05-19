fcf38d34622acf63b0d5a8fd4e2beddc336a09d7
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-05-19 15:53:14 +0100

    Add fonts, styles and favicons

--- rollenspieltage.ch/config.ts
+++ rollenspieltage.ch/config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   site: "https://rollenspieltage.ch",
   srcDir: "./rollenspieltage.ch",
+  publicDir: "./rollenspieltage.ch/public",
   server: {
     port: 1113
   },
   devToolbar: {
     enabled: false
+  },
+  vite: {
+    plugins: [viteStaticCopy({
+      targets: [
+        {
+          src: "./common/fonts/*",
+          dest: "./fonts/"
+        }
+      ]
+    })]
   }
 });
