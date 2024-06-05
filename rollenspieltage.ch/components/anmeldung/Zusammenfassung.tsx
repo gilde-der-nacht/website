6a0e5166c20731c56d878eea3c8b11fbb7442602
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-06-05 22:14:20 +0100

    RST: Meine Anmeldung, Show tentative Reservations

--- rollenspieltage.ch/components/anmeldung/Zusammenfassung.tsx
+++ rollenspieltage.ch/components/anmeldung/Zusammenfassung.tsx
@@ -1,12 +1,80 @@
 import type { JSX } from "solid-js";
 import type { Program, Save } from "./data";
-import type { TentativeReservation } from "./store";
+import type { TentativeReservation as Reservation } from "./store";
+import { Box } from "@common/components/Box";
+
+function ReservationItem(props: {
+  reservation: Reservation;
+  program: Program | null;
+  protagonistName: string;
+  type?: "success" | "danger" | "special" | "gray";
+}): JSX.Element {
+  const game = props.program?.gameList.find(
+    (game) => game.uuid === props.reservation.gameUuid,
+  );
+
+  if (game === undefined || props.program === null) {
+    return (
+      <Box type="danger">
+        Leider ist ein Fehler aufgetreten. Bitte lade die Seite neu oder melde
+        dich bei uns per Kontaktformluar.
+      </Box>
+    );
+  }
+
+  return (
+    <Box type={props.type ?? "gray"}>
+      <p>
+        <strong>
+          {game.title === null ? game.system : `${game.title} (${game.system})`}{" "}
+        </strong>
+        | {game.slot.day === "SATURDAY" ? "Samstag" : "Sonntag"},{" "}
+        {game.slot.start} bis {game.slot.end} Uhr
+      </p>
+      <p>
+        Reservation für
+        <strong>
+          {props.reservation.friendsName === null
+            ? props.protagonistName
+            : props.reservation.friendsName}
+        </strong>
+      </p>
+    </Box>
+  );
+}
+
+function groupReservationByDay(
+  reservations: Reservation[],
+  program: Program | null,
+): { SATURDAY: Reservation[]; SUNDAY: Reservation[] } {
+  const SATURDAY: Reservation[] = [];
+  const SUNDAY: Reservation[] = [];
+
+  for (const reservation of reservations) {
+    const game = program?.gameList.find(
+      (game) => game.uuid === reservation.gameUuid,
+    );
+    if (game?.slot.day === "SATURDAY") {
+      SATURDAY.push(reservation);
+    }
+    if (game?.slot.day === "SUNDAY") {
+      SUNDAY.push(reservation);
+    }
+  }
+
+  return { SATURDAY, SUNDAY };
+}
 
 export function Zusammenfassung(props: {
   save: Save;
-  tentativeReservations: TentativeReservation[];
+  tentativeReservations: Reservation[];
   program: Program | null;
 }): JSX.Element {
+  const tentativeReservationsGrouped = groupReservationByDay(
+    props.tentativeReservations,
+    props.program,
+  );
+
   return (
     <div class="content">
       <h2>Zusammenfassung</h2>
@@ -36,11 +104,40 @@ export function Zusammenfassung(props: {
             Bitte speichere deinen aktuellen Stand der Anmeldung, damit wir die
             folgenden Reservationen bestätigen können.
           </p>
-          <ul>
-            {props.tentativeReservations.map((reservation) => (
-              <li>{reservation.gameUuid}</li>
-            ))}
-          </ul>
+          {tentativeReservationsGrouped.SATURDAY.length > 0 ? (
+            <>
+              <h5>Samstag, 24. August 2024</h5>
+              <ul role="list" style="display: grid; gap: .5rem;">
+                {tentativeReservationsGrouped.SATURDAY.map((reservation) => (
+                  <li>
+                    <ReservationItem
+                      reservation={reservation}
+                      program={props.program}
+                      protagonistName={props.save.name}
+                      type="special"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : null}
+          {tentativeReservationsGrouped.SUNDAY.length > 0 ? (
+            <>
+              <h5>Sonntag, 25. August 2024</h5>
+              <ul role="list" style="display: grid; gap: .5rem;">
+                {tentativeReservationsGrouped.SUNDAY.map((reservation) => (
+                  <li>
+                    <ReservationItem
+                      reservation={reservation}
+                      program={props.program}
+                      protagonistName={props.save.name}
+                      type="special"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : null}
         </>
       ) : null}
       <br />
