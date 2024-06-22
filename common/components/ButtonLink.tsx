ba827474b32572450bd8a313b8fa6f4b52411a7f
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-06-23 00:12:22 +0100

    RST: Update content for registration 2024

--- common/components/ButtonLink.tsx
+++ common/components/ButtonLink.tsx
@@ -3,11 +3,15 @@ import { type JSX } from "solid-js";
 type Props = {
   link: string;
   label: string;
+  kind?: "accent" | "special" | "gray" | "success" | "danger";
 };
 
 export function ButtonLink(props: Props): JSX.Element {
   return (
-    <a href={props.link} class="button button-small button-special">
+    <a
+      href={props.link}
+      class={`button button-small button-${props.kind ?? "special"}`}
+    >
       <i class="fa-duotone fa-arrow-turn-down-right event-icon"></i>
       {props.label}
     </a>
