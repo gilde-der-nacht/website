43786bef31a9f9ed1478926f7790ee8d5822e6e2
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2024-09-26 20:25:01 +0100

    Remove script

--- hhh.gildedernacht.ch/components/webapp/script.sh
+++
@@ -1,2 +0,0 @@
-find . -type f -name "*.tsx" -print0 | xargs -0 sed -i "" -e "s/@/@hhh\/components\/webapp\//g"
-find . -type f -name "*.ts" -print0 | xargs -0 sed -i "" -e "s/@/@hhh\/components\/webapp\//g"
