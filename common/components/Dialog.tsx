05114b2b81503870de6b12774fa8ee3cd6c46a8f
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2025-05-25 15:21:27 +0200

    RST: Add Dialog component

--- common/components/Dialog.tsx
+++ common/components/Dialog.tsx
@@ -1,5 +1,37 @@
-import type { JSX } from "solid-js";
+import { createEffect, Show, type JSX } from "solid-js";
+import { createStore, type Store } from "solid-js/store";
 
-export function Dialog(props: { children: JSX.Element }): JSX.Element {
-  return <dialog open>{props.children}</dialog>;
+export type DialogType = "success" | "danger" | "special" | "gray" | "warning";
+
+export function Dialog(props: {
+  store: Store<{ open: boolean }>;
+  title?: string;
+  type?: DialogType;
+  children: JSX.Element;
+}): JSX.Element {
+  let dialogEl: HTMLDialogElement | undefined = undefined;
+  const [store, setStore] = createStore(props.store);
+
+  createEffect(() => {
+    if (store.open) {
+      (dialogEl as unknown as HTMLDialogElement).showModal();
+    } else {
+      (dialogEl as unknown as HTMLDialogElement).close();
+    }
+  });
+
+  function close() {
+    setStore({ open: false });
+  }
+
+  return (
+    <dialog
+      ref={dialogEl}
+      onClose={close}
+      class={`dialog-${props.type ?? "gray"}`}
+    >
+      <Show when={props.title}>{(title) => <h4>{title()}</h4>}</Show>
+      {props.children}
+    </dialog>
+  );
 }
--- rollenspieltage.ch/components/anmeldung/Router.tsx
+++ rollenspieltage.ch/components/anmeldung/Router.tsx
@@ -28,7 +28,6 @@ import { createStore, type Store } from "solid-js/store";
 import { PageTemplate } from "@rst/components/anmeldung/pages/PageTemplate";
 import { loadRegistrations } from "@rst/components/anmeldung/api/registrations";
 import { loadProgram } from "@rst/components/anmeldung/api/program";
-import { Dialog } from "@common/components/Dialog";
 
 function initPage(meta: Store<MetaClient>): void {
   const url = new URL(location.href);
@@ -102,9 +101,6 @@ export function Router(props: {
         <Box type="success">{TXT.registrationStarted}</Box>
         <br />
       </Show>
-      <Dialog>
-        <h1>oh yea</h1>
-      </Dialog>
       <Switch fallback={<ChoosePage changePage={changePage} />}>
         <Match when={store.meta.page.kind === "PLAYER"}>
           <PlayerPage changePage={changePage} />
