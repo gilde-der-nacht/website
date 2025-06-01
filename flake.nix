929f351f7d842a754624761bc50ab7c9d3f89615
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2025-06-01 17:47:33 +0200

    Environment: add nodejs

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             yarn-berry
+            nodejs
             just
             emmet-ls
             marksman
