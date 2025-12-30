4a4b440336f96a2c159e25e1315936fded6b04ac
Level8Broccoli <7040739+Level8Broccoli@users.noreply.github.com>
2025-12-30 22:20:55 +0100

    Add ics file

---
+++ gildedernacht.ch/build.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import path from "path";
+
+fetch("https://elysium.gildedernacht.ch/calendar/v2/gilde.ics").then(
+  async (result) => {
+    if (result.ok) {
+      fs.writeFile(
+        path.join(process.cwd(), "gildedernacht.ch/public", "test.ics"),
+        await result.text(),
+        () => {},
+      );
+    }
+  },
+);
