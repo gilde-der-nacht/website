4f3d994531e5d5d600c392fba51b5318d955b237
Level8Broccoli <7040739+Level8Broccoli@users.noreply.github.com>
2026-01-12 13:40:53 +0100

    LST: Add Freies Spielen Seite

---
+++ common/components/Heading.tsx
@@ -0,0 +1,34 @@
+import { type JSX } from "solid-js";
+import { Dynamic } from "solid-js/web";
+import type { WithChildren } from "./utils";
+
+type Level = 1 | 2 | 3 | 4;
+
+function toKebabCase(str: string): string {
+  return encodeURIComponent(str.replaceAll(/\s/g, "-").toLocaleLowerCase());
+}
+
+type HeadingProps = {
+  level: Level;
+} & (
+  | {
+      title: string;
+      id?: string;
+    }
+  | (WithChildren & {
+      id: string;
+    })
+);
+
+export function Heading(props: HeadingProps): JSX.Element {
+  const id = toKebabCase(
+    "title" in props ? (props.id ?? props.title) : props.id,
+  );
+  return (
+    <Dynamic component={`h${props.level}`} id={id}>
+      <a href={`#${id}`} class="header-anchor">
+        {"title" in props ? props.title : props.children}
+      </a>
+    </Dynamic>
+  );
+}
