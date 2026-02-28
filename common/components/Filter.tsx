fbd3638d7b397f02af12c28b87ec64a85695d952
Level8Broccoli <7040739+Level8Broccoli@users.noreply.github.com>
2026-02-28 18:36:04 +0100

    LST: Add filter to programm page

--- common/components/Filter.tsx
+++ common/components/Filter.tsx
@@ -1,7 +1,8 @@
 import { Box } from "@common/components/Box";
 import { Button } from "@common/components/Button";
+import { arr, obj, type Reactive } from "@common/utils/reactivity";
 import type { ProgramDay } from "@common/utils/time";
-import { Show, type Accessor, type JSX, type Setter } from "solid-js";
+import { For, Show, type Accessor, type JSX, type Setter } from "solid-js";
 
 export type DayFilterState = ProgramDay | null;
 
@@ -45,6 +46,107 @@ export function DayFilter(props: {
           />
         </Show>
       </div>
+    </Box>
+  );
+}
+
+export type ActiveFilter = {
+  tags: string[];
+  day: DayFilterState;
+};
+
+type FilterUpdater = {
+  toggleDay: (day: DayFilterState) => void;
+  toggleTag: (tag: string) => void;
+  toggleAllTags: () => void;
+};
+
+function createFilterUpdater($filters: Reactive<ActiveFilter>): FilterUpdater {
+  return {
+    toggleDay: (day) => {
+      const isActive = $filters.get().day === day;
+      if (isActive) {
+        $filters.pipe(obj.sub("day")).set(null);
+      } else {
+        $filters.pipe(obj.sub("day")).set(day);
+      }
+    },
+    toggleTag: (tag) => {
+      const isActive = $filters.get().tags.includes(tag);
+      if (isActive) {
+        $filters
+          .pipe(obj.sub("tags"))
+          .set($filters.get().tags.filter((t) => t !== tag));
+      } else {
+        arr.push($filters.pipe(obj.sub("tags")), tag);
+      }
+    },
+    toggleAllTags: () => {
+      $filters.pipe(obj.sub("tags")).set([]);
+    },
+  };
+}
+
+export function Filters(props: {
+  $filters: Reactive<ActiveFilter>;
+  tags: { label: string; name: string }[];
+}): JSX.Element {
+  const updater = createFilterUpdater(props.$filters);
+  return (
+    <Box>
+      <div style="display: flex; flex-wrap: wrap; gap: 1rem; justify-content: space-between;">
+        <h5 style="margin: 0;">Filter</h5>
+      </div>
+      <h6 style="margin-block: 0.5rem;">Tage</h6>
+      <div style="display: flex; flex-wrap: wrap; gap: 0.5rem;">
+        <Button
+          label="Alle Tage"
+          kind={props.$filters.get().day === null ? "success" : "gray"}
+          onClick={() => updater.toggleDay(null)}
+        />
+        <Button
+          label="Samstag"
+          kind={props.$filters.get().day !== "SUNDAY" ? "success" : "gray"}
+          onClick={() => updater.toggleDay("SATURDAY")}
+        />
+        <Button
+          label="Sonntag"
+          kind={props.$filters.get().day !== "SATURDAY" ? "success" : "gray"}
+          onClick={() => updater.toggleDay("SUNDAY")}
+        />
+      </div>
+      <h6 style="margin-block: 0.5rem;">Kategorien </h6>
+      <div style="display: flex; flex-wrap: wrap; gap: 0.5rem;">
+        <Button
+          label="Alle Kategorien"
+          kind={props.$filters.get().tags.length === 0 ? "success" : "gray"}
+          onClick={() => updater.toggleAllTags()}
+        />
+        <For each={props.tags}>
+          {(categories) => (
+            <Button
+              label={categories.label}
+              kind={
+                props.$filters.get().tags.length === 0 ||
+                props.$filters.get().tags.includes(categories.name)
+                  ? "success"
+                  : "gray"
+              }
+              onClick={() => {
+                const newTagFilter = new Set(
+                  props.$filters.get().tags.concat(categories.name),
+                );
+                if (newTagFilter.size === props.tags.length) {
+                  updater.toggleAllTags();
+                } else {
+                  updater.toggleTag(categories.name);
+                }
+              }}
+            />
+          )}
+        </For>
+      </div>
+      {}
     </Box>
   );
 }
--- spieltage.ch/components/anmeldung/pages/Programm.tsx
+++ spieltage.ch/components/anmeldung/pages/Programm.tsx
@@ -1,11 +1,18 @@
 import { Box } from "@common/components/Box";
-import type { Reactive } from "@common/utils/reactivity";
+import { createReactive, type Reactive } from "@common/utils/reactivity";
 import { For, Show, Suspense, type JSX, type Resource } from "solid-js";
 import type { Save } from "@lst/components/anmeldung/api/save";
 import type { Result } from "@lst/components/anmeldung/api/elysium";
-import type { Public } from "@lst/components/anmeldung/api/public";
+import type {
+  Public,
+  PublicProgramEntry,
+} from "@lst/components/anmeldung/api/public";
 import { TXT } from "@common/utils/texts";
 import { Entry } from "@lst/components/anmeldung/components/Entry";
+import { toRange, type PerDay, type ProgramDay } from "@common/utils/time";
+import { Filters, type ActiveFilter } from "@common/components/Filter";
+import { getDay } from "../constant/time";
+import { Heading } from "@common/components/Heading";
 
 export function Programm(props: {
   save$: Reactive<Save>;
@@ -34,11 +41,125 @@ export function Programm(props: {
 }
 
 function ProgramView(props: { save: Save; publicState: Public }): JSX.Element {
+  const $filters = createReactive<ActiveFilter>({
+    day: null,
+    tags: [],
+  });
+
+  const program = () =>
+    filterSortGroupProgram(props.publicState.programEntries, $filters);
+
+  const tags = new Set(
+    props.publicState.programEntries.flatMap((entry) =>
+      entry.tagNames
+        .split(",")
+        .map((s) => s.trim())
+        .filter((s) => s.length !== 0),
+    ),
+  );
+
   return (
-    <ul role="list" class="event-list">
-      <For each={props.publicState.programEntries}>
-        {(entry) => <Entry entry={entry} basePath="/programm" />}
+    <>
+      <Filters
+        $filters={$filters}
+        tags={[...tags].toSorted().map((tag) => ({ label: tag, name: tag }))}
+      />
+      <br />
+      <Show
+        when={$filters.get().day === "SATURDAY" || $filters.get().day === null}
+      >
+        <Heading level={3} title="Samstag" />
+        <DayProgram day="SATURDAY" program={program().SATURDAY} />
+        <br />
+      </Show>
+      <Show
+        when={$filters.get().day === "SUNDAY" || $filters.get().day === null}
+      >
+        <Heading level={3} title="Sonntag" />
+        <DayProgram day="SUNDAY" program={program().SUNDAY} />
+      </Show>
+    </>
+  );
+}
+
+type HourProgram = { hour: number; entries: PublicProgramEntry[] };
+
+function filterSortGroupProgram(
+  entries: PublicProgramEntry[],
+  $filters: Reactive<ActiveFilter>,
+): PerDay<HourProgram[]> {
+  const filters = $filters.get();
+  const days: PerDay<HourProgram[]> = {
+    FRIDAY: [],
+    SATURDAY: [],
+    SUNDAY: [],
+  };
+
+  entries.forEach((entry) => {
+    if (
+      filters.tags.length !== 0 &&
+      !new Set(filters.tags).isSubsetOf(
+        new Set(entry.tagNames.split(",").map((s) => s.trim())),
+      )
+    ) {
+      return;
+    }
+
+    const day = getDay(entry.slot.day);
+    if (day === null) {
+      return;
+    }
+    const startHour = entry.slot.start.hour;
+
+    const found = days[day].find((hour) => hour.hour === startHour);
+    if (found === undefined) {
+      days[day].push({
+        hour: startHour,
+        entries: [entry],
+      });
+    } else {
+      found.entries.push(entry);
+    }
+  });
+
+  return days;
+}
+
+export function DayProgram(props: {
+  day: ProgramDay;
+  program: HourProgram[];
+}): JSX.Element {
+  return (
+    <Show
+      when={props.program.length > 0}
+      fallback={
+        <>
+          <br />
+          <Box>
+            Keine Spielrunden mit den ausgewählten Filtern am{" "}
+            {TXT.days[props.day]} gefunden.
+          </Box>
+        </>
+      }
+    >
+      <For each={toRange(24)}>
+        {(hour) => (
+          <Show when={props.program.find((h) => h.hour === hour)}>
+            {(hourProgram) => (
+              <>
+                <h4 style="margin-block-start: 2rem; margin-block-end: 1rem;">
+                  Start: {hour} Uhr
+                </h4>
+                <ul role="list" class="event-list">
+                  <For each={hourProgram().entries}>
+                    {(entry) => <Entry entry={entry} basePath="/programm" />}
+                  </For>
+                </ul>
+              </>
+            )}
+          </Show>
+        )}
       </For>
-    </ul>
+    </Show>
   );
 }
